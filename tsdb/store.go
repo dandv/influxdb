801a37888c610e117f4398913fb74c66bea95191
Adam Perlin <adamp@nanosoft.com>
2019-07-09 17:03:08 -0700

    set limit back

--- tsdb/store.go
+++ tsdb/store.go
@@ -290,7 +290,7 @@ func (s *Store) loadShards() error {
 	defer logEnd()
 
 	fmt.Println("go max procs: ", runtime.GOMAXPROCS(0))
-	l := runtime.GOMAXPROCS(0) // runtime.GOMAXPROCS(0) / 2
+	l := runtime.GOMAXPROCS(0) / 2
 	fmt.Println("setting limiter to use ", l)
 
 	t := limiter.NewFixed(l)
